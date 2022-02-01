6f50e28a23eeabaadd7b60dab6c1792c673a33d9
Jason <technology@jasonwhite.me>
2022-02-01 13:55:50 -0800

    Allow all Hosts

--- mediamanager/settings.py
+++ mediamanager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8f^+m(z6kpwvjhp-yofsv83#!#^8pl*7k2otolgxhap18x(e77
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
