7a68cdb3e3692ac3307dd467c2fbe664960ebcbb
Jason <technology@jasonwhite.me>
2022-02-01 15:02:23 -0800

    Added basic scanner test

---
+++ scan-test.py
@@ -0,0 +1,43 @@
+import requests, sys
+from pathlib import Path
+
+arg_line = " ".join(sys.argv[1:])
+
+target_uri = "http://localhost:8000/ffmpeg/getMediaInfo"
+
+#scan_dir = '/Users/Jason/Projects/mediamanager'
+scan_dir = '/mnt/media/Movie/Apex (2021)/'
+
+    
+scan_dir = arg_line[0]
+
+valid_extensions = [
+    '.mp4',
+    '.m4v',
+    '.avi',
+    '.mpg',
+    '.mpeg'
+]
+
+#recursive scan of all files
+pathlist = Path(scan_dir).glob('**/*.*')
+for path in pathlist:
+    # because path is object not string
+    
+    for extension in valid_extensions:
+        if str(path.suffix) == extension:
+            path_in_str = str(path)
+            post_data = {"target": path_in_str}
+            response = requests.get(target_uri, data = post_data)
+            print(response.json())
+
+#for target in scan_targets:
+
+    #post_data = {
+    #    "target": target
+    #}
+
+    #response = requests.get(target_uri, data = post_data)
+
+    #print(response.json())
+
