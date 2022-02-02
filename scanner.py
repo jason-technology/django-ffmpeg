94e0df95d932f7ef31d9307a61f4f33ec7e7868b
Jason <technology@jasonwhite.me>
2022-02-01 17:37:27 -0800

    debug

---
+++ scanner.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+#scan_dir = '/Users/Jason/Projects/mediamanager'
+scan_dir = '/mnt/media/Television/'
+
+extensions = []
+
+def try_loop(g):
+    while True:
+        try:
+            yield next(g)
+        except StopIteration:
+            break
+            #continue
+            #pass
+        except OSError as e:
+            # log error
+            continue
+
+for file in try_loop(Path(scan_dir).glob('**/*.*')):
+    print(file)
+    extension = str(file.suffix)
+    if extension not in extensions:
+        extensions.append(extension)
+        
+
+for extension in extensions:
+    print(extension)
+
+    
