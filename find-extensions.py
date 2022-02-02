55ba6cfc97c50edcabc4c6288f9ccc3e19493b89
Jason <technology@jasonwhite.me>
2022-02-01 16:13:53 -0800

    Scan fix

--- find-extensions.py
+++ find-extensions.py
@@ -3,13 +3,28 @@ from pathlib import Path
 #scan_dir = '/Users/Jason/Projects/mediamanager'
 scan_dir = '/mnt/media/Movie/'
 
-extensions = []
+def try_loop(g):
+    while True:
+        try:
+            yield next(g)
+        except StopIteration:
+            break
+        except OSError as e:
+            # log error
+            continue
+
+#for file in try_loop(pathlib.Path(r'\\path\to\shared\folder').rglob('*.foo')):
+for file in try_loop(Path(scan_dir).rglob('**/*.*')):
+    print(file)
+    pass
+
+#extensions = []
 
 #recursive scan of all files
-pathlist = Path(scan_dir).glob('**/*.*')
-for path in pathlist:
+#pathlist = Path(scan_dir).glob('**/*.*')
+#for path in pathlist:
     # because path is object not string
-    print(path)
+#    print(path)
     #extension = str(path.suffix)
     #if extension not in extensions:
     #    extensions.append(extension)
