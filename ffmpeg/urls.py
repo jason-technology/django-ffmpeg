7d53fe397e48f6e9a24e9078ca176f2af4149ac1
Jason <technology@jasonwhite.me>
2022-02-02 10:57:41 -0800

    Added URL for new view

--- ffmpeg/urls.py
+++ ffmpeg/urls.py
@@ -5,6 +5,7 @@ from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 urlpatterns = [
     path("ffmpeg/version", views.FFMPEGVersion.as_view()),
     path("mediafiles/", views.MediaFiles.as_view()),
+    path("invalidmediafiles/", views.InvalidMediaFiles.as_view()),
     path("mediafiles/<int:pk>", views.MediaDetail.as_view()),
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="docs"),
