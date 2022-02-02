f42d597b4347494af6b95513fc033fd1dddfbac4
Jason <technology@jasonwhite.me>
2022-02-02 11:02:25 -0800

    Added admin view for invalid file model

--- ffmpeg/admin.py
+++ ffmpeg/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import MediaFile
+from .models import MediaFile, InvalidMediaFile
 admin.site.register(MediaFile)
+admin.site.register(InvalidMediaFile)
